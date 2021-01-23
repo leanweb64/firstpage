bf4af5e5663f523da9fcebccc64251af5f8cf8e4
leanweb64 <leanweb64@gmail.com>
2021-01-22 23:54:04 -0300

    para subir a github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <div className='Header'>
+         <h1 className='title'>Ushuaia</h1>
+        <ul className='list'>
+          <li>Home</li>
+          <li>Hotel</li>
+          <li>Excursion</li>
+          <li>Pics</li>
+        </ul>
+       </div>
+
+       
+    
     </div>
   );
 }
